932270ed9d7db67f51dc634123c5db7addf12f69
naim3450 <naim.saim74@gmail.com>
2024-11-07 01:21:39 +0600

    responsive up

--- src/Layout/Hero.jsx
+++ src/Layout/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
             <div className="">
                 <h1 className='font-bold font-sans xl:text-[70px] text-title xl:leading-[85px] text-[8vw] sm:text-[5vw] md:text-[6vw] leading-[9vw] sm:leading-[50px] md:leading-[7vw] w-[85vw] sm:w-[350px] md:w-[50vw] xl:w-[680px] text-left'>We Are Ready To Make Your New Life Happier.</h1>
                 <p
-                    className='xl:w-[40vw] w-[50vw] text-left text-[2vw] leading-[3vw] md:text-[1.9vw] xl:text-[1.5vw] xl:!leading-[2.1vw] text-cmgc xl:my-[30px] my-[15px]'>
+                    className='xl:w-[40vw] w-[50vw] text-left max-sm:text-[2.5vw] sm:text-[2vw] leading-[3vw] md:text-[1.9vw] xl:text-[1.5vw] xl:!leading-[2.1vw] text-cmgc xl:my-[30px] my-[15px]'>
                     We work with several leading insurance companies to secure insurance coverages tailored to meet your needs.
                 </p>
                 <Cmbutton text={`Get a consultation`} className={`xl:!w-[246px] md:!w-[30vw] w-[180px] mt-[10px]`} />
